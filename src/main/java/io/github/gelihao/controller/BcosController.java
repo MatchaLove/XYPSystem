a02f8ca17cd6663783ed65d55558798881e0ef9b
MatchaLove <1930399051@qq.com>
2022-04-16 16:06:29 +0800

    区块链连接成功

--- src/main/java/io/github/gelihao/controller/BcosController.java
+++ src/main/java/io/github/gelihao/controller/BcosController.java
@@ -1,33 +1,33 @@
-package io.github.gelihao.controller;
-
-import org.fisco.bcos.sdk.BcosSDK;
-import org.fisco.bcos.sdk.client.Client;
-import org.fisco.bcos.sdk.client.protocol.response.BlockNumber;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-@ResponseBody
-@RestController
-public class BcosController {
-    @GetMapping("/test")
-    public String test(){
-        System.out.println("-----test------");
-        return "this is bcos demo";
-    }
-    @GetMapping("/block")
-    public String getBlockNumber(){
-        System.out.println("-----getBlockNumber getBlockNumber------");
-        @SuppressWarnings("resource")
-        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:fisco-config.xml");
-        System.out.println("-----getBlockNumber ClassPathXmlApplicationContext ok------");
-        BcosSDK bcosSDK = context.getBean(BcosSDK.class);
-        System.out.println("-----getBlockNumber BcosSDK ok------");
-        Client client = bcosSDK.getClient(Integer.valueOf(1));
-        System.out.println("-----getBlockNumber client ok------");
-        BlockNumber blockNumber = client.getBlockNumber();
-        return "getBlockNumber: "+blockNumber.getBlockNumber().toString();
-//        return "";
-    }
-}
+//package io.github.gelihao.controller;
+//
+//import org.fisco.bcos.sdk.BcosSDK;
+//import org.fisco.bcos.sdk.client.Client;
+//import org.fisco.bcos.sdk.client.protocol.response.BlockNumber;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.RestController;
+//@ResponseBody
+//@RestController
+//public class BcosController {
+//    @GetMapping("/test")
+//    public String test(){
+//        System.out.println("-----test------");
+//        return "this is bcos demo";
+//    }
+//    @GetMapping("/block")
+//    public String getBlockNumber(){
+//        System.out.println("-----getBlockNumber getBlockNumber------");
+//        @SuppressWarnings("resource")
+//        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:fisco-config.xml");
+//        System.out.println("-----getBlockNumber ClassPathXmlApplicationContext ok------");
+//        BcosSDK bcosSDK = context.getBean(BcosSDK.class);
+//        System.out.println("-----getBlockNumber BcosSDK ok------");
+//        Client client = bcosSDK.getClient(Integer.valueOf(1));
+//        System.out.println("-----getBlockNumber client ok------");
+//        BlockNumber blockNumber = client.getBlockNumber();
+//        return "getBlockNumber: "+blockNumber.getBlockNumber().toString();
+////        return "";
+//    }
+//}
--- src/test/java/io/github/gelihao/BcosSDKTest.java
+++ src/test/java/io/github/gelihao/BcosSDKTest.java
@@ -1,15 +1,18 @@
 package io.github.gelihao;
 
+import io.github.gelihao.contract.HelloWorld;
 import org.fisco.bcos.sdk.BcosSDK;
 import org.fisco.bcos.sdk.client.Client;
 import org.fisco.bcos.sdk.client.protocol.response.BlockNumber;
 import org.fisco.bcos.sdk.config.exceptions.ConfigException;
+import org.fisco.bcos.sdk.crypto.keypair.CryptoKeyPair;
+import org.fisco.bcos.sdk.model.TransactionReceipt;
 
 public class BcosSDKTest
 {
     // 获取配置文件路径
     public final String configFile = BcosSDKTest.class.getClassLoader().getResource("config.toml").getPath();
-    public void testClient() throws ConfigException {
+    public void testClient() throws Exception {
 
         BcosSDK sdk =  BcosSDK.build(configFile);
 
@@ -18,14 +21,18 @@ public class BcosSDKTest
         BlockNumber blockNumber = client.getBlockNumber();
         System.out.println(blockNumber.getBlockNumber());
 
-//        CryptoKeyPair cryptoKeyPair = client.getCryptoSuite().getCryptoKeyPair();
-//        HelloWorld helloWorld = HelloWorld.deploy(client, cryptoKeyPair);
-//
-//        // 调用HelloWorld合约的get接口
-//        String getValue = helloWorld.get();
-//
-//        // 调用HelloWorld合约的set接口
-//        TransactionReceipt receipt = helloWorld.set("Hello, fisco");
+        CryptoKeyPair cryptoKeyPair = client.getCryptoSuite().getCryptoKeyPair();
+        HelloWorld helloWorld = HelloWorld.deploy(client, cryptoKeyPair);
+
+        // 调用HelloWorld合约的get接口
+        String getValue = helloWorld.get();
+        System.out.println(getValue);
+        System.out.println();
+        // 调用HelloWorld合约的set接口
+        TransactionReceipt receipt = helloWorld.set("Hello, fisco2");
+        getValue = helloWorld.get();
+        System.out.println(getValue);
+        System.out.println();
     }
 
     public static void main(String[] args) throws Exception{
